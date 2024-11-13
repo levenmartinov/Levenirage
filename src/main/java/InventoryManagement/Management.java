e8daa60bb2a542d93c07329dfedf6b9afb0d0c07
levenmartinov <EmailAdresiniz>
2024-11-13 21:38:34 +0000

    levenirage1

---
+++ src/main/java/InventoryManagement/Management.java
@@ -0,0 +1,2 @@
+package InventoryManagement;public interface Management {
+}
--- src/main/java/InventoryManagement/Product.java
+++ src/main/java/InventoryManagement/Product.java
@@ -1,2 +1,52 @@
-package InventoryManagement;public class Product {
+package InventoryManagement;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Product {
+
+    private String sku; //stock keeping unit
+    public static int counter = loadCounter(); //sku counter
+    private static final String COUNTER_FILE = "counter.txt"; //sku safe file
+
+    private String productName;
+    private String supplierName;
+    private int quantity;
+    private String unit;
+    private int shelf;
+
+    public Product(String sku, String productName, String supplierName, int quantity, String unit, int shelf) {
+        this.sku = sku;
+        this.productName = productName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.shelf = shelf;
+    }
+
+    //Static method that loads counter value from file
+    public static int loadCounter() {
+
+        try {
+
+            if (Files.exists(Paths.get(COUNTER_FILE))) {
+
+                BufferedReader reader = new BufferedReader(new FileReader(COUNTER_FILE));
+                String line = reader.readLine();
+                reader.close();
+                return Integer.parseInt(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 10000;
+
+    }
+
+
+
 }
